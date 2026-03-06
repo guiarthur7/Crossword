75d3df2de108d49c90c3c4c662b9eea54cb01a39
eryan <ryan.elqali@ynov.com>
2026-03-06 10:41:40 +0100

    first

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-function aaa() {
-    
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.question("Entrer votre grille ? ", function(grid) {
+    rl.question("Quels sont vos mots ? (format exact : mot1, mot2, mot3). ", function(word) {
+        const words = split(word);
+        console.log(`${grid}, contient les mots : ${words.join(", ")}`);
+        rl.close();
+    });
+});
+rl.on("close", function() {
+    console.log("\nAu revoir !");
+    process.exit(0);
+})
+
+
+function split(str) {
+    let result = [];
+    let separator = ",";
+    let word = "";
+    for (let i = 0 ; i < str.length; i++) {
+        if (str[i] === separator) {
+            result.push(word);
+            word = "";
+            i++
+        } else {
+            word += str[i];
+        }
+    }
+    result.push(word);
+    return result;
+}
+
+function grid(puzzle) {
+    const lines = puzzle.split('\n');
+    let res = [];
+    for (let i = 0; i < lines.length; i++) {
+        res.push(lines[i]);
+    }
+    return [res];
 }
